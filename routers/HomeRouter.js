a68ea91b673e11634c166df759e14b7a6796e643
Darrell Hubbard <thehubbard89@gmail.com>
2025-08-26 14:39:01 -0400

    comment on session design

--- routers/HomeRouter.js
+++ routers/HomeRouter.js
@@ -5,4 +5,18 @@ router.get('/', (req, res) => {
     res.render('home', { title: 'Home Page' });
 });
 
+/*
+    session example
+    req.session.user = { 
+        name: 'John Doe',
+        exam: { 
+            examId: 123, 
+            currentQuestion: 1, 
+            questions: [], // load all questions here
+            answerSheet: [],
+            totalQuestions: questions.length
+            }
+        };
+*/
+
 module.exports = router;
